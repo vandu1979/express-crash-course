f95dd116f83a7f14c4715319e6f7fb1bfac2bc63
vandu1979 <vandana_kothari@yahoo.com>
2022-11-04 11:51:35 -0600

    Hi

--- index.js
+++ index.js
@@ -1,9 +1,36 @@
 const express = require('express');
+const path = require('path');
+const exphbs = require('express-handlebars');
+const logger = require('./middleware/logger');
+const members = require('./Members');
 
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send('<h1')
+
+// Init middleware
+//app.use(logger);
+
+//handlebars Middleware
+app.engine('handlebars', exphbs.engine({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+// Body Parser Middleware
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+
+//Homepage Route
+app.get('/', (req, res) => res.render('index', {
+    title: 'Member App',
+    members
 })
+);
+
+//set static folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Members API Routes
+app.use('/api/members', require('./routes/api/members'));
+
     const PORT = process.env.PORT || 5000;
+
     app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
